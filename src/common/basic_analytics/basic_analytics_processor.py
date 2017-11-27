befb047b757e4784b6b52b98130c15dd6bcc852f
Nikolai Kostenko <Nikolai_Kostenko@epam.com>
2017-11-27 15:27:12 +0300

    Deleted useless udf

--- src/common/basic_analytics/basic_analytics_processor.py
+++ src/common/basic_analytics/basic_analytics_processor.py
@@ -58,8 +58,6 @@ class BasicAnalyticsProcessor:
         return [self.__convert_to_kafka_structure(result) for results in aggregated_results for result in results]
 
     def __convert_to_kafka_structure(self, dataframe):
-        utc = tz.gettz('UTC')
-        func = udf(lambda x: x.astimezone(utc).isoformat())
         return dataframe \
             .withColumn("@timestamp", col("window.start")) \
             .drop("window") \
