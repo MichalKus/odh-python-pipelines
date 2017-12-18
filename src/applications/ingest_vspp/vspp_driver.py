b9b94edc72154d4fb1c24c0270be1e9aa179f944
Alexey_Suprun <Alexey_Suprun@epam.com>
2017-12-18 15:35:13 +0300

    sonar: fixed smell codes

--- src/applications/ingest_vspp/vspp_driver.py
+++ src/applications/ingest_vspp/vspp_driver.py
@@ -9,53 +9,53 @@ from util.utils import Utils
 from applications.ingest_vspp.vspp_udf import VsppUDF
 
 
-def createSparkSession(configuration):
+def create_spark_session(configuration):
     return SparkSession.builder \
         .appName(configuration.property("spark.appName")) \
         .master(configuration.property("spark.master")) \
         .getOrCreate()
 
 
-def setKafkaSettings(configuration, stream, topicOption, topic, ):
+def set_kafka_settings(configuration, stream, topic_option, topic, ):
     options = configuration.property("kafka")
     return stream.option("kafka.bootstrap.servers", options["bootstrap.servers"]) \
-        .option(topicOption, topic) \
+        .option(topic_option, topic) \
         .option("startingOffsets", options["startingOffsets"]) \
         .option("kafka.security.protocol", options["security.protocol"]) \
         .option("kafka.sasl.mechanism", options["sasl.mechanism"])
 
 
-def createReadStream(configuration, spark):
+def create_read_stream(configuration, spark):
     readStream = spark.readStream.format("kafka")
-    return setKafkaSettings(configuration, readStream, "subscribe", configuration.property("kafka")["topic.input"]) \
+    return set_kafka_settings(configuration, readStream, "subscribe", configuration.property("kafka")["topic.input"]) \
         .load()
 
 
-def createPipeline(readStream):
+def create_pipeline(read_stream):
     schema = StructType().add("message", StringType())
-    resultSchema = ArrayType(StructType([
+    result_schema = ArrayType(StructType([
         StructField("@timestamp", TimestampType(), False),
         StructField("processing_speed", DoubleType(), False)
     ]))
-    vsppUdf = udf(VsppUDF.process, resultSchema)
-    return readStream \
-        .select(from_json(readStream["value"].cast("string"), schema).alias("value")) \
-        .withColumn("result", explode(vsppUdf("value.message"))) \
+    vspp_udf = udf(VsppUDF.process, result_schema)
+    return read_stream \
+        .select(from_json(read_stream["value"].cast("string"), schema).alias("value")) \
+        .withColumn("result", explode(vspp_udf("value.message"))) \
         .select("result.@timestamp", "result.processing_speed") \
         .withColumn("type", lit("airflow_ingest_network_performance_vspp")) \
         .selectExpr("to_json(struct(*)) AS value")
 
 
-def createWriteStream(configuration, pipeline):
-    writeStream = pipeline.writeStream.format("kafka")
-    return setKafkaSettings(configuration, writeStream, "topic", configuration.property("kafka")["topic.output"]) \
+def create_write_stream(configuration, pipeline):
+    write_stream = pipeline.writeStream.format("kafka")
+    return set_kafka_settings(configuration, write_stream, "topic", configuration.property("kafka")["topic.output"]) \
         .option("checkpointLocation", configuration.property("spark.checkpointLocation"))
 
 
 if __name__ == "__main__":
     configuration = Utils.load_config(sys.argv[:])
-    spark = createSparkSession(configuration)
-    readStream = createReadStream(configuration, spark)
-    pipeline = createPipeline(readStream)
-    writeStream = createWriteStream(configuration, pipeline)
-    writeStream.start().awaitTermination()
+    spark = create_spark_session(configuration)
+    read_stream = create_read_stream(configuration, spark)
+    pipeline = create_pipeline(read_stream)
+    write_stream = create_write_stream(configuration, pipeline)
+    write_stream.start().awaitTermination()
