a28ebc060b8c9a9cca1df6ee4ee2eed335e14f5f
Lev_Khacheresiantc <Lev_Khacheresiantc@epam.com>
2017-12-19 12:37:54 +0300

    Added description to Airflow driver. Added description to Stagis driver.

--- src/applications/log_parsing/stagis/driver.py
+++ src/applications/log_parsing/stagis/driver.py
@@ -11,83 +11,106 @@ from common.log_parsing.timezone_metadata import ConfigurableTimestampField
 from util import Utils
 
 
-def concat(item1, item2): return item1 + item2
-
-
-def take_first(item1, item2): return item1
-
-
 def create_event_creators(configuration):
+    """
+    Create MatchField configuration for Stagis topics that macth patterns:
+    - *stagis_log_gen*
+    - *stagis_log_err*
+    - *stagis_corecommit_log*
+    - *stagis_interface_log*
+    - *stagis_wcf_log*
+    :param configuration: job configuration
+    :return: initialized MatchField configuration for Stagis component
+    """
     timezone_name = configuration.property("timezone.name")
     timezones_priority = configuration.property("timezone.priority", "dic")
 
     return MatchField("topic", {
         "stagis_log_gen": SourceConfiguration(
-            stagis_ee_event_creator(timezone_name, timezones_priority),
+            Stagis.ee_event_creator(timezone_name, timezones_priority),
             Utils.get_output_topic(configuration, "general")
         ),
         "stagis_log_err": SourceConfiguration(
-            stagis_ee_event_creator(timezone_name, timezones_priority),
+            Stagis.ee_event_creator(timezone_name, timezones_priority),
             Utils.get_output_topic(configuration, "error")
         ),
         "stagis_corecommit_log": SourceConfiguration(
-            stagis_ee_corecommit_event_creator(timezone_name, timezones_priority),
+            Stagis.ee_corecommit_event_creator(timezone_name, timezones_priority),
             Utils.get_output_topic(configuration, "corecommit")
         ),
         "stagis_interface_log": SourceConfiguration(
-            stagis_ee_interface_event_creator(timezone_name, timezones_priority),
+            Stagis.ee_interface_event_creator(timezone_name, timezones_priority),
             Utils.get_output_topic(configuration, "interface")
         ),
         "stagis_wcf_log": SourceConfiguration(
-            stagis_ee_wcf_event_creator(timezone_name, timezones_priority),
+            Stagis.ee_wcf_event_creator(timezone_name, timezones_priority),
             Utils.get_output_topic(configuration, "wcf")
         )
     })
 
 
-def stagis_ee_event_creator(timezone_name, timezones_priority):
-    return EventCreator(Metadata([
-        ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
-        StringField("level"),
-        StringField("instance_name"),
-        StringField("causality_id"),
-        StringField("thread_id"),
-        StringField("class_name"),
-        StringField("message")
-    ]),
-        SplitterParser("|", is_trim=True))
+class Stagis:
+    """
+    Contains methods for creation EventCreator for specific logs
+    """
 
+    @staticmethod
+    def ee_event_creator(timezone_name, timezones_priority):
+        return EventCreator(
+            Metadata([
+                ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
+                StringField("level"),
+                StringField("instance_name"),
+                StringField("causality_id"),
+                StringField("thread_id"),
+                StringField("class_name"),
+                StringField("message")
+            ]),
+            SplitterParser("|", is_trim=True))
 
-def stagis_ee_corecommit_event_creator(timezone_name, timezones_priority):
-    return EventCreator(Metadata([
-        ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
-        StringField("level"),
-        StringField("instance_name"),
-        StringField("causality_id"),
-        StringField("thread_id"),
-        StringField("class_name"),
-        StringField("message")
-    ]),
-        SplitterParser("|", is_trim=True))
+    @staticmethod
+    def ee_corecommit_event_creator(timezone_name, timezones_priority):
+        return EventCreator(
+            Metadata([
+                ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
+                StringField("level"),
+                StringField("instance_name"),
+                StringField("causality_id"),
+                StringField("thread_id"),
+                StringField("class_name"),
+                StringField("message")
+            ]),
+            SplitterParser("|", is_trim=True))
 
+    @staticmethod
+    def ee_interface_event_creator(timezone_name, timezones_priority):
+        return EventCreator(
+            Metadata([
+                ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
+                StringField("instance_name"),
+                StringField("thread_id"),
+                StringField("class_name"),
+                StringField("message")
+            ]),
+            SplitterParser("|", is_trim=True))
 
-def stagis_ee_interface_event_creator(timezone_name, timezones_priority):
-    return EventCreator(Metadata([
-        ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
-        StringField("instance_name"),
-        StringField("thread_id"),
-        StringField("class_name"),
-        StringField("message")
-    ]),
-        SplitterParser("|", is_trim=True))
+    @staticmethod
+    def ee_wcf_event_creator(timezone_name, timezones_priority):
+        def concat(item1, item2):
+            """  method concatenates params """
+            return item1 + item2
 
+        def take_first(item1, item2):
+            """  method takes left parameter only """
+            return item1
 
-def stagis_ee_wcf_event_creator(timezone_name, timezones_priority):
-    return MultilineEventCreator(Metadata([
-        ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
-        StringField("message")
-    ]),
-        SplitterParser("|", is_trim=True), [take_first, concat])
+        return MultilineEventCreator(
+            Metadata([
+                ConfigurableTimestampField("@timestamp", timezone_name, timezones_priority),
+                StringField("message")
+            ]),
+            SplitterParser("|", is_trim=True), [take_first, concat])
+
 
 if __name__ == "__main__":
     configuration = Utils.load_config(sys.argv[:])
