51602e56012135f07ba3b8524fcdf709b1828cee
Sergey Ermolaev <sermolaev.1986@gmail.com>
2018-02-07 13:06:12 +0300

    ODH-1680 EOS STB Errors report. Network and connectivity ODH-1685 EOS STB Display report - renamed driver files

--- src/test/it/applications/basic_analytics/eos_stb/test_display.py
+++ src/test/it/applications/basic_analytics/eos_stb/test_display.py
@@ -1,4 +1,4 @@
-from applications.basic_analytics.stb_analytics.display import create_processor
+from applications.basic_analytics.stb_analytics.display_report_driver import create_processor
 from test.it.core.base_spark_test_case import BaseSparkProcessorTestCase
 
 
