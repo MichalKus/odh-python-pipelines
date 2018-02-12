30f7df996161f2cc40bf908f0abe19e28788d30e
nilan3 <nilanthanb1994@gmail.com>
2018-02-12 20:44:46 +0000

    add VROPS mem and net stats basic analytics job

---
+++ src/test/it/applications/basic_analytics/vrops/test_vrops_vm_mem_net_basic_analytics.py
@@ -0,0 +1,11 @@
+from applications.basic_analytics.vrops.vm_mem_net_driver import create_processor
+from test.it.core.base_spark_test_case import BaseSparkProcessorTestCase
+
+class VropsVMCpuBasicAnalyticsTestCase(BaseSparkProcessorTestCase):
+    def test_events_success(self):
+        self._test_pipeline(
+            configuration_path="test/it/resources/basic_analytics/vrops/vm_mem_net_stats/configuration.yml",
+            processor_creator=create_processor,
+            input_dir="test/it/resources/basic_analytics/vrops/vm_mem_net_stats/input",
+            expected_result_file="test/it/resources/basic_analytics/vrops/vm_mem_net_stats/expected_result.txt"
+        )
