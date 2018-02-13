cb6ba2d4e15b1dcc68ee9a5f0910210a3e569024
nilan3 <nilanthanb1994@gmail.com>
2018-02-13 08:44:20 +0000

    clean up

--- src/test/it/applications/basic_analytics/vrops/test_vrops_vm_mem_net_basic_analytics.py
+++ src/test/it/applications/basic_analytics/vrops/test_vrops_vm_mem_net_basic_analytics.py
@@ -1,7 +1,7 @@
 from applications.basic_analytics.vrops.vm_mem_net_driver import create_processor
 from test.it.core.base_spark_test_case import BaseSparkProcessorTestCase
 
-class VropsVMCpuBasicAnalyticsTestCase(BaseSparkProcessorTestCase):
+class VropsVMMemNetBasicAnalyticsTestCase(BaseSparkProcessorTestCase):
     def test_events_success(self):
         self._test_pipeline(
             configuration_path="test/it/resources/basic_analytics/vrops/vm_mem_net_stats/configuration.yml",
