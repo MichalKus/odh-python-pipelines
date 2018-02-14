09b59172898154faee91da2a6774bd83e82c5176
Nikolai Kostenko <Nikolai_Kostenko@epam.com>
2018-02-14 17:56:17 +0300

    Added extention for traxis_frontend log parsing.

--- src/common/log_parsing/dict_event_creator/event_creator.py
+++ src/common/log_parsing/dict_event_creator/event_creator.py
@@ -36,43 +36,59 @@ class EventCreator(AbstractEventCreator):
         return self.__field_to_parse
 
 
+class ParserStep:
+    """
+    One step in CompositeEventCreator, contains event_creator and two flags about dependency from previous step
+    and necessity to do next parsing
+    """
+
+    def __init__(self, event_creator, dependent, final):
+        self.event_creator = event_creator
+        self.dependent = dependent
+        self.final = final
+
+
 class CompositeEventCreator:
     def __init__(self):
         self.__event_creator_list = list()
 
-    def add_source_parser(self, event_creator):
+    def add_source_parser(self, event_creator, final=False):
         """
         Event creator added using this method uses source event that event produced on previous step.
         For example: original message is {'message':'event is 115566"}, intermediate result that we got from
         previous parser is {'message':'event is 115566', 'event_id':'event is 115566'}. For event creator added using
         this method is used original message as input result.
 
+        :param final: Flag shows if we should continue process next event creators or not
         :param event_creator: class that are responsible for parsing
         """
-        self.__event_creator_list.append((False, event_creator))
+        self.__event_creator_list.append(ParserStep(event_creator, dependent=False, final=final))
         return self
 
-    def add_intermediate_result_parser(self, event_creator):
+    def add_intermediate_result_parser(self, event_creator, final=True):
         """
         Event creator added using this method uses event that produced by previous step rather than source event.
         For example: original message is {'message':'event is 115566"}, intermediate result that we got from
         previous parser is {'message':'event is 115566', 'event_id':'event is 115566'}. For event creator added using
         this method is used intermediate result as input.
 
+        :param final: Flag shows if we should continue process next event creators or not
         :param event_creator: class that are responsible for parsing
         """
-        self.__event_creator_list.append((True, event_creator))
+        self.__event_creator_list.append(ParserStep(event_creator, dependent=True, final=final))
         return self
 
     def create(self, source_message):
         intermediate_message = {}
-
-        for dependent, event_creator in self.__event_creator_list:
-
-            if dependent:
-                if event_creator.get_field_to_parse() in intermediate_message:
-                    intermediate_message.update(event_creator.create(intermediate_message))
+        result = {}
+        for parserStep in self.__event_creator_list:
+            if parserStep.dependent:
+                if parserStep.event_creator.get_field_to_parse() in intermediate_message:
+                    result = parserStep.event_creator.create(intermediate_message)
+                    intermediate_message.update(result)
             else:
-                intermediate_message.update(event_creator.create(source_message))
-
+                result = parserStep.event_creator.create(source_message)
+                intermediate_message.update(result)
+            if result and parserStep.final:
+                break
         return intermediate_message
