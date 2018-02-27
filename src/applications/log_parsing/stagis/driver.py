467cfeedbf97250c74673e7f7cb4c6ac54450f7e
Sergey Ermolaev <sermolaev.1986@gmail.com>
2018-02-27 13:29:10 +0300

    ODH-1835 Extend Stagis parser - Task info - (supposedly) fixed Pickle error

--- src/applications/log_parsing/stagis/driver.py
+++ src/applications/log_parsing/stagis/driver.py
@@ -62,22 +62,22 @@ class StagisEventCreator(EventCreator):
     Replaces extracted task value with the one from dictionary
     """
 
-    __dictionary = {
-        "TVA Delta Server respond": "TVA Delta Server response",
-        "TVA Delta Request Starting": "TVA Delta Server request",
-        "Received Delta Server Notification": "Notification",
-        "Model state after committing transaction": "Committing Transaction"
-    }
-
     def _convert_row_to_event_values(self, row):
         parse_result = self._parser.parse(row["message"])
         if parse_result:
             parse_result[0] = self.__replace_task_name(parse_result[0])
         return parse_result
 
-    def __replace_task_name(self, text):
-        if text in self.__dictionary:
-            text = text.replace(text, self.__dictionary[text])
+    @staticmethod
+    def __replace_task_name(text):
+        dictionary = {
+            "TVA Delta Server respond": "TVA Delta Server response",
+            "TVA Delta Request Starting": "TVA Delta Server request",
+            "Received Delta Server Notification": "Notification",
+            "Model state after committing transaction": "Committing Transaction"
+        }
+        if text in dictionary:
+            text = text.replace(text, dictionary[text])
         return text
 
 
