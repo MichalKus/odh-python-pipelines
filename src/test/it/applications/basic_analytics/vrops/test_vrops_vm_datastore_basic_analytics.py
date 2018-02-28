a5ad3070ebe18547dad2a9eb6dc68ec7b3708934
nilan3 <nilanthanb1994@gmail.com>
2018-02-28 08:54:50 +0000

    updated datastore job and added it test

--- src/test/it/applications/basic_analytics/vrops/test_vrops_vm_datastore_basic_analytics.py
+++ src/test/it/applications/basic_analytics/vrops/test_vrops_vm_datastore_basic_analytics.py
@@ -1,7 +1,7 @@
 from applications.basic_analytics.vrops.vm_datastore_driver import create_processor
 from test.it.core.base_spark_test_case import BaseSparkProcessorTestCase
 
-class VropsVMDatastoreDiskBasicAnalyticsTestCase(BaseSparkProcessorTestCase):
+class VropsVMDatastoreBasicAnalyticsTestCase(BaseSparkProcessorTestCase):
     def test_events_success(self):
         self._test_pipeline(
             configuration_path="test/it/resources/basic_analytics/vrops/vm_datastore_stats/configuration.yml",
