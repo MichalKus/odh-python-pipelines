dee00c9618a934b76a977f580249a3309f47de58
Alexey Suprun <Alexey_Suprun@epam.com>
2018-03-06 16:03:01 +0300

    ODH-1763: added ConnectivityTestReport fields to Network and Connectivity errors report

--- src/applications/basic_analytics/eos_stb_reports/network_errors_report_driver.py
+++ src/applications/basic_analytics/eos_stb_reports/network_errors_report_driver.py
@@ -1,13 +1,14 @@
 """
 Basic analytics driver for STB Network and Connectivity errors.
 """
-from common.basic_analytics.basic_analytics_processor import BasicAnalyticsProcessor
-from util.kafka_pipeline_helper import start_basic_analytics_pipeline
+from pyspark.sql.functions import col
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType
+
 from common.basic_analytics.aggregations import Count, Sum, Max, Min, Stddev, CompoundAggregation
 from common.basic_analytics.aggregations import P01, P05, P10, P25, P50, P75, P90, P95, P99
-from pyspark.sql.functions import col
+from common.basic_analytics.basic_analytics_processor import BasicAnalyticsProcessor
 from common.spark_utils.custom_functions import convert_epoch_to_iso
+from util.kafka_pipeline_helper import start_basic_analytics_pipeline
 
 
 class NetworkErrorsStbBasicAnalytics(BasicAnalyticsProcessor):
@@ -18,11 +19,19 @@ class NetworkErrorsStbBasicAnalytics(BasicAnalyticsProcessor):
     __dimensions = ["hardwareVersion", "firmwareVersion", "appVersion", "asVersion"]
 
     def _process_pipeline(self, json_stream):
-        stream = json_stream\
-            .withColumn("Ethernet_TxErrors", col("EthernetStats_txErrors").cast(IntegerType()))\
-            .withColumn("Ethernet_RxErrors", col("EthernetStats_rxErrors").cast(IntegerType()))\
-            .withColumn("Wifi_TxErrors", col("WiFiStats_txErrors").cast(IntegerType()))\
-            .withColumn("Wifi_RxErrors", col("WiFiStats_rxErrors").cast(IntegerType()))
+        stream = json_stream \
+            .withColumn("Ethernet_TxErrors", col("EthernetStats_txErrors").cast(IntegerType())) \
+            .withColumn("Ethernet_RxErrors", col("EthernetStats_rxErrors").cast(IntegerType())) \
+            .withColumn("Wifi_TxErrors", col("WiFiStats_txErrors").cast(IntegerType())) \
+            .withColumn("Wifi_RxErrors", col("WiFiStats_rxErrors").cast(IntegerType())) \
+            .withColumn(
+                "ConnectivityTestReport_downstreamBitrate_error",
+                col("ConnectivityTestReport_downstreamBitrate").cast(IntegerType())
+            ) \
+                .withColumn(
+                "ConnectivityTestReport_upstreamBitrate_error",
+                col("ConnectivityTestReport_upstreamBitrate").cast(IntegerType())
+            )
 
         aggregation_fields = ["Ethernet_TxErrors", "Ethernet_RxErrors", "Wifi_TxErrors", "Wifi_RxErrors"]
         result = []
@@ -35,10 +44,26 @@ class NetworkErrorsStbBasicAnalytics(BasicAnalyticsProcessor):
             aggregations = [Sum(**kwargs), Count(**kwargs), Max(**kwargs), Min(**kwargs), Stddev(**kwargs),
                             P01(**kwargs), P05(**kwargs), P10(**kwargs), P25(**kwargs), P50(**kwargs),
                             P75(**kwargs), P90(**kwargs), P95(**kwargs), P99(**kwargs)]
-
             result.append(stream.aggregate(CompoundAggregation(aggregations=aggregations, **kwargs)))
 
+        self.__append_connectivity_fields(stream, result)
         return result
+
+    def __append_connectivity_fields(self, stream, result):
+        connectivity_fields = [
+            "ConnectivityTestReport_downstreamBitrate_error",
+            "ConnectivityTestReport_upstreamBitrate_error"
+        ]
+        for field in connectivity_fields:
+            result.append(
+                stream
+                    .where(col(field) == "-1")
+                    .aggregate(Count(
+                        group_fields=self.__dimensions,
+                        aggregation_field=field,
+                        aggregation_name=self._component_name
+                    ))
+            )
 
     def _prepare_timefield(self, data_stream):
         return convert_epoch_to_iso(data_stream, "timestamp", "@timestamp")
@@ -55,6 +80,8 @@ class NetworkErrorsStbBasicAnalytics(BasicAnalyticsProcessor):
             StructField("EthernetStats_rxErrors", StringType()),
             StructField("WiFiStats_txErrors", StringType()),
             StructField("WiFiStats_rxErrors", StringType()),
+            StructField("ConnectivityTestReport_downstreamBitrate", StringType()),
+            StructField("ConnectivityTestReport_upstreamBitrate", StringType()),
         ])
 
 
