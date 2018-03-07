de8c8ab7662b90723f64cffe616c5dc87e191edc
Alexey Suprun <Alexey_Suprun@epam.com>
2018-03-07 12:13:14 +0300

    ODH-1984: nokiavrm remove scheduler_bs_audit test

--- src/test/unit/applications/log_parsing/nokia_vrm/test_nokia_vrm_log_parsing.py
+++ src/test/unit/applications/log_parsing/nokia_vrm/test_nokia_vrm_log_parsing.py
@@ -6,26 +6,6 @@ from test.unit.core.base_message_parsing_test_cases import BaseMultipleMessagePa
 
 class NokiaVrmMessageParsingTestCase(BaseMultipleMessageParsingTestCase):
     event_creators = create_event_creators()
-
-    def test_scheduler_bs_audit(self):
-        self.assert_parsing(
-            {
-                "source": "/opt/vrm/jetty-scheduler-BS/logs/scheduler_bs_audit.log",
-                "message": "29-Oct-2017 21:49:12.672 | INFO  | 0409217a-7120-4afd-8939-49c142749ab8 | INTERNAL |  | [[GET] /scheduler/web/Record/addByProgram] | [{schema=1.0, eventId=crid:~~2F~~2Ftelenet.be~~2Fd3cb0337-2cad-45a5-9252-5ad7711354e8,imi:0010000000075BC2, userId=03e30b70-106c-416b-8243-40fe1e7f981b_be}] | Entity not found (info) | Not found Event entity with programId field equals to crid:~~2F~~2Ftelenet.be~~2Fd3cb0337-2cad-45a5-9252-5ad7711354e8,imi:0010000000075BC2"
-            },
-            {
-                "@timestamp": datetime(2017, 10, 29, 21, 49, 12, 672000),
-                "level": "INFO",
-                "event_id": "0409217a-7120-4afd-8939-49c142749ab8",
-                "domain": "INTERNAL",
-                "ip": "",
-                "method": "[[GET] /scheduler/web/Record/addByProgram]",
-                "params": "[{schema=1.0, eventId=crid:~~2F~~2Ftelenet.be~~2Fd3cb0337-2cad-45a5-9252-5ad7711354e8,imi:0010000000075BC2, userId=03e30b70-106c-416b-8243-40fe1e7f981b_be}]",
-                "description": "Entity not found (info)",
-                "message": "Not found Event entity with programId field equals to crid:~~2F~~2Ftelenet.be~~2Fd3cb0337-2cad-45a5-9252-5ad7711354e8,imi:0010000000075BC2"
-            }
-        )
-
     def test_console_bs_audit(self):
         self.assert_parsing(
             {
