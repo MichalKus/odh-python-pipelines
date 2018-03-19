5576168e1311767b8d067ecde6be92b7ca1738c4
Aleksandr Chermenin <aleksandr_chermenin@epam.com>
2018-03-19 16:11:32 +0300

    ODH-2085: Created driver for F5

--- src/applications/log_parsing/f5/driver.py
+++ src/applications/log_parsing/f5/driver.py
@@ -5,7 +5,13 @@ Spark driver for parsing STB F5 messages
 import sys
 
 from common.kafka_pipeline import KafkaPipeline
+from common.log_parsing.dict_event_creator.event_creator import CompositeEventCreator, EventCreator
+from common.log_parsing.dict_event_creator.key_value_parser import KeyValueParser
+from common.log_parsing.dict_event_creator.regexp_parser import RegexpParser
+from common.log_parsing.dict_event_creator.single_type_event_creator import SingleTypeEventCreator
 from common.log_parsing.log_parsing_processor import LogParsingProcessor
+from common.log_parsing.metadata import StringField, Metadata
+from common.log_parsing.timezone_metadata import ConfigurableTimestampField
 from util.utils import Utils
 
 
@@ -19,7 +25,26 @@ def create_event_creators(config):
     timezone_name = config.property("timezone.name")
     timezones_priority = config.property("timezone.priority", "dic")
 
-    return None
+    message_general_event_creator = SingleTypeEventCreator(
+        StringField(None),
+        KeyValueParser(",", "=", values_strip='"')
+    )
+
+    request_header_event_creator = SingleTypeEventCreator(
+        StringField(None),
+        KeyValueParser("\\r\\n", ":", skip_parsing_exceptions=True),
+        field_to_parse="request_header"
+    )
+
+    timestamp_event_creator = EventCreator(
+        Metadata([ConfigurableTimestampField("timestamp", timezone_name, timezones_priority, "@timestamp")]),
+        RegexpParser(r"(?P<timestamp>\d+)"), field_to_parse="EOCTimestamp"
+    )
+
+    return CompositeEventCreator() \
+        .add_source_parser(message_general_event_creator) \
+        .add_intermediate_result_parser(request_header_event_creator) \
+        .add_intermediate_result_parser(timestamp_event_creator)
 
 
 if __name__ == "__main__":
--- src/common/log_parsing/dict_event_creator/key_value_parser.py
+++ src/common/log_parsing/dict_event_creator/key_value_parser.py
@@ -4,15 +4,19 @@ from common.spark_utils.custom_functions import convert_to_underlined
 
 class KeyValueParser:
 
-    def __init__(self, sequence_separator, key_value_separator):
+    def __init__(self, sequence_separator, key_value_separator,
+                 keys_strip=None, values_strip=None, skip_parsing_exceptions=False):
         self.__sequence_separator = sequence_separator
         self.__key_value_separator = key_value_separator
+        self.__keys_strip = keys_strip
+        self.__values_strip = values_strip
+        self.__skip_parsing_exceptions = skip_parsing_exceptions
 
     def __trim_and_transform_to_key_value(self, key_value):
         pair = key_value.split(self.__key_value_separator)
-        if len(pair) != 2 | pair[0].isspace():
+        if (len(pair) != 2 or pair[0].isspace()) and not self.__skip_parsing_exceptions:
             raise ParsingException("Can't parse message!")
-        return convert_to_underlined(pair[0].strip()), pair[1].strip()
+        return convert_to_underlined(pair[0].strip(self.__keys_strip)), pair[1].strip(self.__values_strip)
 
     def parse(self, text):
         """
@@ -21,4 +25,5 @@ class KeyValueParser:
         :return: parsed dict
         :raises: ParsingException when text doesn't confirm key/value format
         """
-        return dict(filter(lambda value: value[1], map(self.__trim_and_transform_to_key_value, text.split(self.__sequence_separator))))
+        return dict(filter(lambda value: value[1],
+                           map(self.__trim_and_transform_to_key_value, text.split(self.__sequence_separator))))
