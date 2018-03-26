68537993b64f3583d78649fdb69dcf32658d0ac1
Ivan_Ivanov1 <mousebreaker@mail.ru>
2018-03-26 14:28:39 +0300

    ODH-2184: driver namedTuple with predicates/metric names replaced with plain dict.

--- src/applications/basic_analytics/vspp/diagnostic_server_driver.py
+++ src/applications/basic_analytics/vspp/diagnostic_server_driver.py
@@ -1,8 +1,6 @@
 """
 The module for the driver to calculate metrics related to vspp data.
 """
-from collections import namedtuple
-
 from pyspark.sql.functions import col
 from pyspark.sql.types import StructField, StructType, TimestampType, StringType
 
@@ -11,8 +9,6 @@ from common.basic_analytics.aggregations import Count
 from common.spark_utils.custom_functions import custom_translate_regex
 from util.kafka_pipeline_helper import start_basic_analytics_pipeline
 
-QueryFilter = namedtuple("QueryFilter", "error_message metric_name")
-
 
 class VsppDiagnosticServer(BasicAnalyticsProcessor):
     """
@@ -20,21 +16,20 @@ class VsppDiagnosticServer(BasicAnalyticsProcessor):
     """
 
     def _process_pipeline(self, read_stream):
-        query_filters = [
-            QueryFilter("error got \d*, expected \d*, packet \d*",
-                        "total_count_of_cc_error"),
-            QueryFilter("Ingest session service error, failed starting session handler, nested error:",
-                        "total_count_of_cc_ingest_session_service_error"),
-            QueryFilter("underflow in input ES",
-                        "total_count_of_cc_corrupt_teletext_index_error")
-        ]
+        mapping = {
+            ".*error got \d*, expected \d*, packet \d*.*":
+                "total_count_of_cc_error",
+            ".*Ingest session service error, failed starting session handler, nested error:.*":
+                "total_count_of_cc_ingest_session_service_error",
+            ".*underflow in input ES.*":
+                "total_count_of_cc_corrupt_teletext_index_error"
+        }
+
         count_cc_errors_stream = read_stream \
             .withColumn("message_type",
                         custom_translate_regex(
                             source_field=col("message"),
-                            mapping=dict(
-                                (".*" + query_filter.error_message + ".*", query_filter.metric_name) for query_filter in
-                                query_filters),
+                            mapping=mapping,
                             default_value="unclassified")) \
             .filter("message_type != 'unclassified'") \
             .aggregate(Count(group_fields=["message_type"],
