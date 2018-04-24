de3a4d8b97bb1530beb09ca91f5e6a8bbab2f7d3
Ivan_Ivanov1 <mousebreaker@mail.ru>
2018-04-24 18:51:42 +0300

    ODH-2375: refactoring.

--- src/applications/basic_analytics/stb_active_report/driver.py
+++ src/applications/basic_analytics/stb_active_report/driver.py
@@ -15,74 +15,89 @@ class StbActiveReportProcessor(BasicAnalyticsProcessor):
 
     def _process_pipeline(self, read_stream):
 
-        """Extract viewerID from handled record -> used in all metrics."""
-        read_stream = read_stream \
+        """Extract viewerID from handled record -> used in all metrics, save as protected field to ease method calls."""
+        self._read_stream = read_stream \
             .withColumn("viewer_id", col("header").getItem("viewerID"))
 
-        count_distinct_active_stb = read_stream \
+        return [self.count_distinct_active_stb(),
+                self.count_distinct_active_stb_ethernet(),
+                self.count_distinct_active_stb_wifi(),
+                self.count_distinct_active_stb_4k_enabled(),
+                self.count_distinct_active_stb_4k_disabled(),
+                self.count_distinct_active_stb_per_model(),
+                self.count_distinct_active_stb_per_hw_version(),
+                self.count_distinct_active_stb_per_fw_version(),
+                self.count_distinct_active_stb_netflix(),
+                self.count_distinct_active_stb_youtube()]
+
+    def count_distinct_active_stb(self):
+        return self._read_stream \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb"))
 
-        count_distinct_active_stb_ethernet = read_stream \
+    def count_distinct_active_stb_ethernet(self):
+        return self._read_stream \
             .withColumn("kbps", col("EthernetStats").getItem("rxKbps")) \
             .where("kbps > 0") \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_ethernet"))
 
-        count_distinct_active_stb_wifi = read_stream \
+    def count_distinct_active_stb_wifi(self):
+        return self._read_stream \
             .withColumn("kbps", col("WiFiStats").getItem("rxKbps")) \
             .where("kbps > 0") \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_wifi"))
 
-        count_distinct_active_stb_4k_enabled = read_stream \
+    def count_distinct_active_stb_4k_enabled(self):
+        return self._read_stream \
             .withColumn("uhd", col("BCMReport").getItem("4Kcontent")) \
             .where("uhd = true") \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_4k_enabled"))
 
-        count_distinct_active_stb_4k_disabled = read_stream \
+    def count_distinct_active_stb_4k_disabled(self):
+        return self._read_stream \
             .withColumn("uhd", col("BCMReport").getItem("4Kcontent")) \
             .where("uhd = false") \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_4k_disabled"))
 
-        count_distinct_active_stb_per_model = read_stream \
+    def count_distinct_active_stb_per_model(self):
+        return self._read_stream \
             .withColumn("model_name", col("header").getItem("modelName")) \
             .aggregate(DistinctCount(group_fields=["model_name"],
                                      aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_per_model"))
 
-        count_distinct_active_stb_per_hw_version = read_stream \
+    def count_distinct_active_stb_per_hw_version(self):
+        return self._read_stream \
             .withColumn("hardware_version", col("header").getItem("hardwareVersion")) \
             .aggregate(DistinctCount(group_fields=["hardware_version"],
                                      aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_per_hw_version"))
 
-        count_distinct_active_stb_per_fw_version = read_stream \
+    def count_distinct_active_stb_per_fw_version(self):
+        return self._read_stream \
             .withColumn("software_versions", explode(col("header").getItem("softwareVersions"))) \
             .withColumn("version", col("software_versions").getItem("version")) \
             .aggregate(DistinctCount(group_fields=["version"],
                                      aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_per_fw_version"))
 
-        count_distinct_active_stb_netflix = read_stream \
+    def count_distinct_active_stb_netflix(self):
+        return self._read_stream \
             .withColumn("provider_id", col("ApplicationsReport").getItem("provider_id")) \
             .where("provider_id = 'netflix'") \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_netflix"))
 
-        count_distinct_active_stb_youtube = read_stream \
+    def count_distinct_active_stb_youtube(self):
+        return self._read_stream \
             .withColumn("provider_id", col("ApplicationsReport").getItem("provider_id")) \
             .where("provider_id = 'youtube'") \
             .aggregate(DistinctCount(aggregation_field="viewer_id",
                                      aggregation_name=self._component_name + ".active_stb_youtube"))
-
-        return [count_distinct_active_stb, count_distinct_active_stb_ethernet, count_distinct_active_stb_wifi,
-                count_distinct_active_stb_4k_enabled, count_distinct_active_stb_4k_disabled,
-                count_distinct_active_stb_per_model, count_distinct_active_stb_per_hw_version,
-                count_distinct_active_stb_per_fw_version,
-                count_distinct_active_stb_netflix, count_distinct_active_stb_youtube]
 
     @staticmethod
     def create_schema():
