450aa256d6654113c155d673162659cd709c6fd6
Egor_Afutin <Egor_Afutin@epam.com>
2018-04-24 16:51:26 +0300

    name fix

--- src/test/it/applications/basic_analytics/stb_general/test_eos_stb_ethernet_wifi_reports_basic_analytics.py
+++ src/test/it/applications/basic_analytics/stb_general/test_eos_stb_ethernet_wifi_reports_basic_analytics.py
@@ -1,8 +1,8 @@
-from applications.basic_analytics.stb_general.cpe_ethernet_wifi_reports import create_processor
+from applications.basic_analytics.stb_general.cpe_ethernet_wifi_reports_driver import create_processor
 from test.it.core.base_spark_test_case import BaseSparkProcessorTestCase
 
 
-class StbGeneralAnalyticsTestCase(BaseSparkProcessorTestCase):
+class StbEthernetWifiReportsAnalyticsTestCase(BaseSparkProcessorTestCase):
     def test_events(self):
         self._test_pipeline(
             configuration_path="test/it/resources/basic_analytics/stb_general/ethernet_wifi_reports/configuration.yml",
