14d26553bc581e8a56e9623566a127d23fea8a6a
Ivan_Ivanov1 <Ivan_Ivanov1@epam.com>
2018-05-08 14:52:38 +0300

    [ODH-2363]: clarified metrics implemented.

--- src/applications/basic_analytics/eos_stb_reports/flat/memory_usage_report_driver.py
+++ src/applications/basic_analytics/eos_stb_reports/flat/memory_usage_report_driver.py
@@ -3,10 +3,11 @@ Basic analytics driver for STB Memory usage report.
 """
 from pyspark.sql.types import StructType, StructField, TimestampType, StringType, LongType
 
-from common.basic_analytics.aggregations import Avg
+from common.basic_analytics.aggregations import Avg, DistinctCount
 from common.basic_analytics.basic_analytics_processor import BasicAnalyticsProcessor
 from common.spark_utils.custom_functions import convert_epoch_to_iso
 from util.kafka_pipeline_helper import start_basic_analytics_pipeline
+from pyspark.sql.functions import col
 
 
 class MemoryUsageReportEventProcessor(BasicAnalyticsProcessor):
@@ -20,21 +21,27 @@ class MemoryUsageReportEventProcessor(BasicAnalyticsProcessor):
     def _process_pipeline(self, read_stream):
 
         memory_usage_report_stream = read_stream \
-            .select("@timestamp", "MemoryUsage.*")
+            .select("@timestamp", "MemoryUsage.*", col("header.viewerID").alias("viewer_id"))
 
         return [self.__avg_memory_used_kb(memory_usage_report_stream),
                 self.__avg_memory_cached_kb(memory_usage_report_stream),
-                self.__avg_memory_free_kb(memory_usage_report_stream)]
+                self.__avg_memory_free_kb(memory_usage_report_stream),
+                self.__distinct_stb_low_memory_usage(memory_usage_report_stream),
+                self.__distinct_stb_med_memory_usage(memory_usage_report_stream),
+                self.__distinct_stb_high_memory_usage(memory_usage_report_stream)]
 
     @staticmethod
     def create_schema():
         return StructType([
             StructField("@timestamp", TimestampType()),
+            StructField("header", StructType([
+                StructField("viewerID", StringType())
+            ])),
             StructField("MemoryUsage", StructType([
                 StructField("ts", LongType()),
-                StructField("usedKb", StringType()),
-                StructField("cachedKb", StringType()),
-                StructField("freeKb", StringType())
+                StructField("usedKb", LongType()),
+                StructField("cachedKb", LongType()),
+                StructField("freeKb", LongType())
             ]))
         ])
 
@@ -55,6 +62,24 @@ class MemoryUsageReportEventProcessor(BasicAnalyticsProcessor):
             .where("freeKb is not NULL") \
             .aggregate(Avg(aggregation_field="freeKb",
                            aggregation_name=self._component_name))
+
+    def __distinct_stb_low_memory_usage(self, read_stream):
+        return read_stream \
+            .where("usedKb < 1468006") \
+            .aggregate(DistinctCount(aggregation_field="viewer_id",
+                                     aggregation_name=self._component_name + ".low"))
+
+    def __distinct_stb_med_memory_usage(self, read_stream):
+        return read_stream \
+            .where("usedKb >= 1468006 and usedKb <= 1677721") \
+            .aggregate(DistinctCount(aggregation_field="viewer_id",
+                                     aggregation_name=self._component_name + ".med"))
+
+    def __distinct_stb_high_memory_usage(self, read_stream):
+        return read_stream \
+            .where("usedKb > 1677721") \
+            .aggregate(DistinctCount(aggregation_field="viewer_id",
+                                     aggregation_name=self._component_name + ".high"))
 
 
 def create_processor(configuration):
